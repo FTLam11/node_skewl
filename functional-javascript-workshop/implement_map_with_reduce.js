08165ec59becaeec0801a949edd73e36f3e6c0d5
frosted_fronks <ryzingsun11@yahoo.com>
2017-03-07 19:46:29 -0800

    Add note about concat

--- functional-javascript-workshop/implement_map_with_reduce.js
+++ functional-javascript-workshop/implement_map_with_reduce.js
@@ -1,6 +1,7 @@
 module.exports = function arrayMap(arr, fn) {
   return arr.reduce(function(result, currentItem) {
-    result.push(fn.call(null, currentItem));
+    result.push(fn.call(null, currentItem)); // push returns length of array
+    // return result.concat(fn.call(null, currentItem)); // fails nested array inputs
     return result;
   }, []);
 };
